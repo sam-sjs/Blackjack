5e2bb9d20063d3e4fefc5178f71cb7b46ac9731c
Sam Skinner <sam.sjskinner@gmail.com>
2021-03-12 11:16:30 +1100

    Can now shuffle the deck to random card order - Can now draw the next card from the deck

--- BlackjackTests/DeckTests.cs
+++ BlackjackTests/DeckTests.cs
@@ -1,5 +1,6 @@
 using Blackjack;
 using Xunit;
+using Xunit.Sdk;
 
 namespace BlackjackTests
 {
@@ -13,6 +14,37 @@ namespace BlackjackTests
             Deck actual = new Deck();
 
             Assert.Equal(expected, actual.StandardDeck.Length);
+        }
+
+        // This test will fail sometimes when card ends up in same location in deck.  How do I test randomness?
+        [Theory]
+        [InlineData(0)]
+        [InlineData(51)]
+        [InlineData(20)]
+        
+        public void GivenShuffle_WhenInputIsNull_ThenShouldReturnArrayWithDifferentOrder(int index)
+        {
+            Deck newDeck = new Deck();
+            string expected = newDeck.StandardDeck[index].Value + newDeck.StandardDeck[index].Suit;
+            
+            newDeck.Shuffle();
+            string actual = newDeck.StandardDeck[index].Value + newDeck.StandardDeck[index].Suit;
+
+            Assert.NotEqual(expected, actual);
+        }
+
+        public void GivenDraw_WhenInputIsNull_ThenShouldReturnFirstCardInDeck()
+        {
+            Deck newDeck = new Deck();
+            string expectedValue = "2";
+            string expectedSuit = "HEARTS";
+
+            Card firstDraw = newDeck.Draw();
+            string actualValue = firstDraw.Value;
+            string actualSuit = firstDraw.Suit;
+
+            Assert.Equal(expectedValue, actualValue);
+            Assert.Equal(expectedSuit, actualSuit);
         }
     }
 }
